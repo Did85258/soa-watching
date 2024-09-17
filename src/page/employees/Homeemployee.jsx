506b7b94e98663fb4f94537352b29927b500a536
Ryker212 <103985410+Ryker212@users.noreply.github.com>
2024-09-17 22:34:30 +0700

    manage emp

--- src/page/employees/Homeemployee.jsx
+++ src/page/employees/Homeemployee.jsx
@@ -1,13 +1,12 @@
 
 import EmployeeNavbar from "../../components/navbar/NavbarEmployee";
 import EmployeeSidebar from "../../components/sidebar/SidebarEmployee";
-import EmployeeHomeContent from "../../components/contents/employeeContents/EmployeeHomeContent";
 export default function HomeEmployee() {
   return (
     <>
       <EmployeeNavbar />
       <EmployeeSidebar />
-      <EmployeeHomeContent />
+      <EmployeeManage />
     </>
   );
 }
